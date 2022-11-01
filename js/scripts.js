15e4654080498354e9b6522084a3c0a65d771970
Student <student@epicodus-3.hsd1.or.comcast.net>
2022-11-01 10:29:57 -0700

    Add some UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// business logic
+
 function AddressBook() {
     this.contacts = {};
     this.currentId = 0;
@@ -35,7 +37,42 @@ function Contact(firstName, lastName, phoneNumber) {
 
 }
 
+
 Contact.prototype.fullName = function () {
     return this.firstName + " " + this.lastName;
 };
+
+// UI 
+
+let addressBook = new AddressBook();
+
+function listContacts(addressBookToDisplay) {
+    let contactsDiv = document.querySelector("div#contacts");
+    contactsDiv.innerText =  null;
+    const ul = document.createElement("ul");
+    Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+      const contact = addressBookToDisplay.findContact(key);
+      const li = document.createElement("li");
+      li.append(contact.fullName());
+      li.setAttribute("id", contact.id);
+      ul.append(li);
+    });
+    contactsDiv.append(ul);
+  }
+  
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const inputtedFirstName = document.querySelector("input#new-first-name").value;
+  const inputtedLastName = document.querySelector("input#new-last-name").value;
+  const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
+  let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+  addressBook.addContact(newContact);
+  listContacts(addressBook);
+}
+
+window.addEventListener("load", function (){
+  document.querySelector("form#new-contact").addEventListener("submit", handleFormSubmission);
+});
+
 
